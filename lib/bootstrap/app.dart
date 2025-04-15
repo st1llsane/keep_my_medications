8a38f320a406ba4538b5009419c61e4de52102f0
st1llsane <notescape262@gmail.com>
2025-04-15 23:13:56 +0200

    chore: change ios app icon and remove unnecessary deps

--- lib/bootstrap/app.dart
+++ lib/bootstrap/app.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:keep_my_notes/domain/cubits/todo_cubit.dart';
-import 'package:keep_my_notes/domain/repos/todo/todo_repository.dart';
 import 'package:keep_my_notes/pages/home_page.dart';
 import 'package:keep_my_notes/shared/theme/theme_colors.dart';
 
@@ -50,10 +47,7 @@ class App extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: BlocProvider(
-        create: (context) => TodoCubit(todoRepo: TodoRepository()),
-        child: const HomePage(),
-      ),
+      home: const HomePage(),
     );
   }
 }
--- lib/domain/repos/note_repository.dart
+++
@@ -1,7 +0,0 @@
-abstract interface class NoteRepository {
-  void create();
-
-  void deleteAt(int index);
-
-  void deleteAll();
-}
